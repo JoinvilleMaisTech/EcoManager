3bcb57ee62b56787cb6ce2a8c7fc07b94e0b45b1
Anaysa Lopes <anaysalopees@gmail.com>
2025-04-14 18:49:10 -0300

    Adicionando projeto EcoManager ao repositório JoinvilleMaisTech

---
+++ src/main/java/EcoManager/EcoManagerApplication.java
@@ -0,0 +1,13 @@
+package EcoManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoManagerApplication.class, args);
+	}
+
+}
